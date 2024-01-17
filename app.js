8973102fac1b808dc0f5b3a65700739247ced6a7
abbas usman <johnmccain2244@gmail.com>
2024-01-17 10:31:37 +0900

    creating mongoose connetion

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const express = require("express");
 const app = express();
 const router = require("./router.js");
 
-// Call MongoDB 
-const db = require("./server").db();
-const mongodb = require("mongodb");
-
 // 1. Enter codes
 app.use(express.static("public"));
 app.use(express.json());
