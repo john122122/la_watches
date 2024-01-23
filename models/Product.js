99ce91fc0259589a333a283b4bd86f31e2a55a93
abbas usman <johnmccain2244@gmail.com>
2024-01-23 18:46:51 +0900

    feat: Develop Product Schema model to work with mongoose

---
+++ models/Product.js
@@ -0,0 +1,1 @@
+const ProductModul = require("../schema/product.model");
---
+++ schema/product.model.js
@@ -0,0 +1,117 @@
+const mongoose = require("mongoose");
+const {
+    product_collection_enums,
+    product_status_enums,
+    product_size_enums,
+    product_volume_enums,
+} = require("../lib/config");
+const Schema = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+    {
+        product_name: {
+            type: String,
+            required: true,
+        },
+        product_collection: {
+            type: String,
+            required: true,
+            enum: {
+                values: product_collection_enums,
+                message: "{VALUES} is not among permitted values",
+            },
+        },
+        product_status: {
+            type: String,
+            required: false,
+            default: "PAUSED",
+            enum: {
+                values: product_status_enums,
+                message: "{VALUES} is not among permitted values",
+            },
+        },
+        product_price: {
+            type: Number,
+            required: true,
+        },
+        product_discount: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        product_left_cnt: {
+            type: Number,
+            required: true,
+        },
+        product_size: {
+            type: String,
+            required: function () {
+                const sized_list = [
+                    "classic",
+                    "electronic",
+                    "mechanic",
+                    "man",
+                    "women",
+                    "gold",
+                    "silver",
+                    "brilliant",
+                    "etc"
+                ];
+                return sized_list.includes(this.product_collection);
+            },
+            default: "normal",
+            enum: {
+                values: product_size_enums,
+                message: "{VALUES} is not among permitted values",
+            },
+        },
+        product_volume: {
+            type: Number,
+            required: function () {
+                return this.product_collection === "material";
+            },
+            default: 1,
+            enum: {
+                values: product_volume_enums,
+                message: "{VALUES} is not among permitted values",
+            },
+        },
+        product_description: {
+            type: String,
+            required: false,
+        },
+        product_images: {
+            type: Array,
+            required: true,
+            default: [],
+        },
+        product_views: {
+            type: Number,
+            required: false,
+            default: 0,
+        },
+        product_likes: {
+            type: Number,
+            required: false,
+            default: 0,
+        },
+        shop_mb_id: {
+            type: Schema.Types.ObjectId,
+            ref: "Member",
+            required: false,
+        },
+    },
+    { timestamps: true }
+);
+
+productSchema.index(
+    {
+        shop_mb_id: 1,
+        product_name: 1,
+        product_size: 1,
+        product_volume: 1,
+    },
+    { unique: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
