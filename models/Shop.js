567c1fb9a50108e0a6a8d550d2ff6d66035a5aa6
abbas usman <johnmccain2244@gmail.com>
2024-05-19 18:32:18 +0900

    feat: Develop update stores member top data by Admin

--- models/Shop.js
+++ models/Shop.js
@@ -2,11 +2,12 @@ const assert = require("assert");
 const Definer = require("../lib/mistake");
 const MemberModel = require("../schema/member.model");
 const Member = require("../models/Member");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
 
 class Shop {
-    constructor() {
-        this.memberModel = MemberModel;
-    }
+  constructor() {
+    this.memberModel = MemberModel;
+  }
 
 async getAllShopData() {
     try {
@@ -16,12 +17,31 @@ async getAllShopData() {
         })
         .exec();
                 
-        assert(result, Definer.general_err1);
-          return result;
-        } catch (err) {
-          throw err;
-        }
-    }
+      assert(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
+  }
+}
+  
+async updateShopByAdminData(update_data) {
+  try {
+    const id = shapeIntoMongooseObjectId(update_data?.id);
+    const result = await this.memberModel
+      .findByIdAndUpdate({ _id: id }, update_data, {
+        runValidators: true,
+        lean: true,
+        returnDocument: "after",
+      })
+      .exec();
+    
+    assert.ok(result, Definer.general_err1);
+    return result;
+  } catch (err) {
+    throw err;
+  }
+}
+  
 }
 
 module.exports = Shop;
