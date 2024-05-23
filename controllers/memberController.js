90befa5a23b3179674b56720cf2602012bc8ed13
abbas usman <johnmccain2244@gmail.com>
2024-05-23 19:12:58 +0900

    feat: Create jwt tokens and enable cookie-parser

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,4 +1,7 @@
+const assert = require("assert");
+const Definer = require("../lib/mistake");
 const Member = require("../models/Member");
+const jwt = require("jsonwebtoken");
 
 let memberController = module.exports;
 
@@ -7,7 +10,13 @@ memberController.signup = async (req, res) => {
         console.log("POST: cont/signup");    
         const data = req.body;      
             member = new Member();
-            new_member = await member.signupData(data);
+        new_member = await member.signupData(data);
+
+        const token = memberController.createToken(result);
+        res.cookie("access_token", token, {
+            maxAge: 6 * 3600 * 1000,
+            httpOnly: true,
+        });
         
         res.json({state: 'success', data: new_member}); 
     } catch (err) {
@@ -21,7 +30,13 @@ memberController.login = async (req, res) => {
         console.log("POST: cont/login");        
         const data = req.body,                  
             member = new Member(),
-            result = await member.loginData(data);  
+            result = await member.loginData(data); 
+       
+        const token = memberController.createToken(result);
+        res.cookie("access_token", token, {
+            maxAge: 6 * 3600 * 1000,
+            httpOnly: true,
+        });
 
         res.json({ state: "success", data: result });    
     } catch (err) {
@@ -35,3 +50,21 @@ memberController.logout = (req, res) => {
     res.send("You'r in logout page"); 
 };
 
+memberController.createToken = (result) => {
+    try {
+        const upload_data = {
+            _id: result._id,
+            mb_nick: result.mb_nick,
+            mb_type: result.mb_type,
+        };
+
+        const token = jwt.sign(upload_data, process.env.SECRET_TOKEN, {
+            expiresIn: "6h",
+        });
+
+        assert.ok(token, Definer.auth_err2);
+        return token;
+    } catch (err) {
+      throw err;
+    }
+};
